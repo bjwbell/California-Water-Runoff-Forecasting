27c592591f675625ef727ad26af772819eaf4103
Bryan Bell <bryan.w.bell@gmail.com>
2011-05-19 21:55:00 -0700

    Removed junk files

--- train_models.sh
+++ train_models.sh
@@ -3,3 +3,6 @@ rm formatted_data.arff
 python read.py
 java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/formatted_data.arff > linear_model.out
 cat linear_model.out
+
+java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/American.arff > linear_model.out
+cat linear_model.out
