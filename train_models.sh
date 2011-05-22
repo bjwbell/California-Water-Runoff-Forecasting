7659e973a46e29007d76adad41eb3fa4d195ee98
Bryan Bell <bryan.w.bell@gmail.com>
2011-05-22 15:07:27 -0700

    Added more models to train_models.sh

--- train_models.sh
+++ train_models.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-rm formatted_data.arff
-python read.py
-java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/formatted_data.arff > linear_model.out
+#rm formatted_data.arff
+#python read.py
+java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/AmericanRiverInfo_ALL.arff > linear_model.out
 cat linear_model.out
 
-java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/American.arff > linear_model.out
-cat linear_model.out
+java -cp weka.jar weka.classifiers.functions.LinearRegression -t /home/bjwbell/California-Water-Runoff-Forecasting/AmericanRiv_Train.arff > linear_model2.out
+cat linear_model2.out
