ebc97a8aa3d0fd3a2570b7005504db9654cd1080
Bryan Bell <bryan.w.bell@gmail.com>
2011-05-23 09:16:17 -0700

    Added trimmed arff files

--- train_models.bat
+++ train_models.bat
@@ -1,8 +1,3 @@
-#!/bin/sh
-#rm formatted_data.arff
-#python read.py
 java -cp weka.jar weka.classifiers.functions.LinearRegression -t AmericanRiverInfo_ALL.arff > linear_model.out
-cat linear_model.out
-
 java -cp weka.jar weka.classifiers.functions.LinearRegression -t AmericanRiv_TrainNoWaterYear.arff > linear_model2.out
-cat linear_model2.out
+echo "done"
